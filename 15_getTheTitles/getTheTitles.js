21fa49315e1faf4cd33bcbf9548cadd383d5bf3e
Victor Yuan <victor.2wy@gmail.com>
2025-06-11 16:22:23 -0700

    oldest

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  return array.map(obj =>  obj.title)
 };
 
+let books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
+
+console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let addedBirthDate = array.map(obj => {
+
+    let age = 0;
+    if (!('yearOfDeath' in obj)) {
+      let currentYear = new Date().getFullYear();
+      age = currentYear - obj.yearOfBirth;
+    } else {
+      age = obj.yearOfDeath - obj.yearOfBirth;
+    }
+    return {...obj, age: age}
+  })
+
+  // sort
+  let sorted = addedBirthDate.sort((a,b) => a.age - b.age)
+  sorted.reverse();
+
+  console.log(sorted);
+  return sorted[0]
 
 };
+
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
