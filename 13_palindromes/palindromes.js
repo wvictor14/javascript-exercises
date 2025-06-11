883881dabe9f581a2febbec979ad6a9a96b15257
Victor Yuan <victor.2wy@gmail.com>
2025-06-11 14:31:18 -0700

    palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const splitted = string.toLowerCase().replaceAll(' ', '').replaceAll(/[^\w\s]/gi, '').split('');
+  
+  console.log('forward' + splitted);
 
+  const reverse = splitted.toReversed();
+ 
+  console.log('reverse' + reverse);
+
+  for (let i = 0; i < splitted.length; i++){
+
+   if ( splitted[i].match(/[.,:!?]/) || reverse[i].match(/[.,:!?]/) ) {
+    continue
+   }
+
+
+    if (splitted[i] !== reverse[i]) return false
+  }
+
+  return true
 };
+
+console.log(palindromes('abc'))
 
 // Do not edit below this line
 module.exports = palindromes;
