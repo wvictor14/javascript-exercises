06ecc50363a7aa6645cf85c8c9ec29e6ae8e2937
Victor Yuan <victor.2wy@gmail.com>
2025-06-11 15:51:39 -0700

    fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (x) {
+  let current = 1;
+  let current_minus_1 = 0;
 
+  if (x < 0) return 'OOPS';
+  if (x == 0) return current_minus_1;
+  if (x == 1) return current;
+
+  for (let i = 1; i < x; i++) {
+   let new_current = current + current_minus_1;
+
+   console.log(new_current);
+   current_minus_1 = current;
+   current = new_current;
+  }
+  
+  return current;
 };
+
+console.log("0" == 0);
 
 // Do not edit below this line
 module.exports = fibonacci;
